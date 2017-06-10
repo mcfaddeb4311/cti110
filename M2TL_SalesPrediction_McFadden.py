ee6de19cd6d04ee758dfee6321b781152d71d8f4
mcfaddeb4311 <mcfaddeb4311@student.faytechcc.edu>
2017-06-09 22:38:00 -0400

    Add files via upload
    First Sales Prediction Program ( 23 % or Sales)

---
+++ M2TL_SalesPrediction_McFadden.py
@@ -0,0 +1,19 @@
+# Get the projected total sales.
+# June 9, 2017
+# CTI-110 M2T1 - Sales Prediction
+# Bobby McFadden
+
+total_sales = float(input('Enter the project sales:'))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+# Display the profit.
+print('The profit is $', format(profit, ',.2f'))
+
+                    
+
+                                
+
+
+
