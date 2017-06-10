32f6065d3490d676025101038337cd7f7eddda85
mcfaddeb4311 <mcfaddeb4311@student.faytechcc.edu>
2017-06-10 00:26:43 -0400

    Add files via upload
     M2HW1_DistanceTraveled_McFadden.py

---
+++ M2HW1_DistanceTraveled_McFadden.py
@@ -0,0 +1,24 @@
+# Find Distance of Car traveling 70 mph for 6,10, and 15 hours.
+# June 9, 2017
+# CTI-110 M2HW1 - Distance Traveled 
+# Bobby McFadden
+
+#Value for speed
+
+speed = 70
+
+
+#Time Assigned
+time1 = 6
+time2 = 10
+time3 = 15
+
+#Distance Traveled
+distance1 = speed * time1
+distance2 = speed * time2
+distance3 = speed * time3
+
+print("Traveling at 70 miles per hour...")
+print("after 6 hours you will have traveled", distance1, "miles.")
+print("after 10 hours you will have traveed", distance2, "miles.")
+print("after 15 hours you will have traveled", distance3, "miles.")
