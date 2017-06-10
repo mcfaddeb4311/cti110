81bcd51261d3246201d07e401678fefdcd7eeec4
mcfaddeb4311 <mcfaddeb4311@student.faytechcc.edu>
2017-06-10 01:01:09 -0400

    Add files via upload
    Tip Tax Total. M2HW2

---
+++ M2HW2_TipTaxTotal_McFadden.py
@@ -0,0 +1,18 @@
+#Calculate the total amount of meal
+#June 10, 2017
+#CTI-110 M2HW2- Tip Tax Total
+#Bobby McFadden
+
+meal = 56.00
+
+tax = 0.07
+
+tip = 0.18
+
+meal = meal + meal * tax
+meal = meal + meal * tip
+
+total = meal
+print ('your meal total is', "%.2f" % total)
+
+
