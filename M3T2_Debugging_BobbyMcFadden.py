1e837423ead7c41df562881a3de2a7640857757e
mcfaddeb4311 <mcfaddeb4311@student.faytechcc.edu>
2017-06-15 15:35:40 -0400

    Add files via upload
    Debugged Python code. (P.115 #4)

---
+++ M3T2_Debugging_BobbyMcFadden.py
@@ -0,0 +1,28 @@
+#Debugging a program.
+#6-15-2017
+#CTI-110 M3T2- Debugging
+#Bobby McFadden
+
+score = int(input('Enter the score:'))
+A_score=90
+B_score=80
+C_score=70
+D_score=60
+F_score=50
+
+if score >= A_score:
+    print ('Your grade is A.')
+
+
+elif score >= B_score:
+        print ('Your score is B.')
+
+elif score >= C_score:
+    print ('Your score is C.')
+    
+elif score >= D_score:
+    print ('Your score is D.')
+
+elif score >= F_score:
+    print ('Your score is F.')
+    
